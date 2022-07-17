c5e6c1b9fbf1e5edbd0688187946e485b3363e20
Taegyun, Ko <taegyun.ko@metervision.io>
2022-07-17 10:16:37 +0900

    서비스시작 index.ts 작성중

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,52 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import express, { Request, Response, NextFunction } from 'express';
+import ResponseSet from './entity/ResponseSet';
+import Utils from './utils';
 
-AppDataSource.initialize().then(async () => {
+const app = express();
+const router = express.Router();
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+var PORT = 3000;
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+app.all('/:usertoken/:path*', (req: Request, res: Response, next: NextFunction) =>
+{
+    const utils = Utils(req);
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    const username = utils.getUsername();
+    const token = utils.getToken();
+    const ip = utils.getIPv4();
 
-}).catch(error => console.log(error))
+    let response: ResponseSet = new ResponseSet();
+
+    if (Utils(req).getMethod() == "get")
+    {
+
+    }
+
+    console.log(ip);
+    console.log("req.user : " + req.params.usertoken);
+    console.log("req.path : " + req.path);
+    console.log("user : " + req.query.token);
+    res.status(200).json({});
+});
+
+app.use(router);
+app.listen(PORT, () => { });
+
+/* AppDataSource.initialize().then(async () =>
+{
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await AppDataSource.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
+
+    console.log("Loading users from the database...");
+    const users = await AppDataSource.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    console.log("Here you can setup and run express / fastify / any other framework.");
+
+}).catch(error => console.log(error));
+ */
